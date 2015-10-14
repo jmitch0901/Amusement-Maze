6fc3806869551d1c70fd078c8aebf0b4dc19f1e8
jmitch <jmitch0901@gmail.com>
2015-10-14 01:32:34 -0500

    Got the 50x50 scene maze!

--- Amuzement Maze/View.cpp
+++ Amuzement Maze/View.cpp
@@ -99,7 +99,7 @@ void View::draw()
 	GLuint a;
 
     modelview.push(glm::mat4(1.0));
-	modelview.top() = modelview.top() * glm::lookAt(glm::vec3(0,250,-80),glm::vec3(0,0,0),glm::vec3(0,1,0)) * trackballTransform;
+	modelview.top() = modelview.top() * glm::lookAt(glm::vec3(0,250,80),glm::vec3(0,0,0),glm::vec3(0,1,0)) * trackballTransform;
 
 	glUniformMatrix4fv(projectionLocation,1,GL_FALSE,glm::value_ptr(proj.top()));
 	/*
